59408a6bc5f4f8fed7cddc7f3781d558966c2a69
sajay <sajaykannan10@gmail.com>
2021-01-22 12:58:29 +0530

    adding languagefundamentls 22/01/2021

---
+++ Languagefundamentals/age.py
@@ -0,0 +1,8 @@
+#create two variable age=25,name="ajay"
+#print ajay is 25 years old
+
+#print()
+#input()
+name=input("enter value for name")
+age=input("enter age")
+print(name,"is",age,"years old")
---
+++ Languagefundamentals/swapping.py
@@ -0,0 +1,15 @@
+num1=10
+num2=20
+# temp=num1#temp=10
+# num1=num2#num1=20
+# num2=temp#num2=10
+# print(num1)#20
+# print(num2)#20
+
+#
+# num1=num1+num2 #num1=10+20=30
+# num2=num1-num2#30-20=10
+# num1=num1-num2#30-10=20
+
+
+# num1,num2=num2,num1
